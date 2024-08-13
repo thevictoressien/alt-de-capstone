bea4551059150b203378c8544d59e1e426dc58b2
Victor Essien <victor.essien@alerzo.com>
2024-08-13 13:47:01 +0100

    Modify load schema to accept GCS URI

--- dags/scripts/bq_utils.py
+++ dags/scripts/bq_utils.py
@@ -1,10 +1,29 @@
 import json
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
+
+
 
 def load_schema(schema_path):
-     with open(schema_path, 'r') as schema_file:
-        schema = json.load(schema_file)
-        return schema
+    if schema_path.startswith("gs://"):
+        # If the path is a GCS URI, use the Google Cloud Storage client to fetch the file
+        storage_client = storage.Client()
+
+        # Extract the bucket name and the blob (file) name from the GCS URI
+        bucket_name = schema_path.split("/")[2]
+        blob_name = "/".join(schema_path.split("/")[3:])
+
+        # Get the bucket and the blob
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        # Download the contents of the blob as a string and parse it as JSON
+        schema_json = blob.download_as_text()
+        schema = json.loads(schema_json)
+    else:
+        # If the path is a local file path, load it directly
+        with open(schema_path, 'r') as schema_file:
+            schema = json.load(schema_file)
+    return schema
 
 
 def fetch_job_config(file_format, schema, field_delimiter, create_disposition='CREATE_IF_NEEDED', write_disposition='WRITE_APPEND'):
